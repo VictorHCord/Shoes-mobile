701deb1b221336631bdf50a7645aed74c4b8a386
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-11-30 19:06:32 -0300

    shoes mobile finished

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import './config/reactotronConfig';
 import { Provider } from 'react-redux';
-import Routes from './routes';
 import store from './store';
+import Routes from './routes';
+import NavigationService from './services/navigation';
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <StatusBar barStyle="light-content" backgroundColor="#000" />
-        <Routes />
+        <Routes 
+        ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
+        />
       </Provider>
     </>
   );
